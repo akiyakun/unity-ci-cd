2ed8277ea515b58a6e72204cf84b3e14a6cdaf1d
akiya <akiyastyle@gmail.com>
2024-09-27 22:14:31 +0900

    Add CommandLineHelper.

---
+++ Packages/com.akiyakun.unitycicd/Editor/CommandLineHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace unicicd.Editor
+{
+    public static class CommandLineHelper
+    {
+        static List<string> additiveArgs;
+
+        // システムのコマンドライン引数とこのクラスが内部で保持しているコマンドライン引数をマージして返します
+        public static List<string> GetCommandLineArgs()
+        {
+            List<string> ret = new(System.Environment.GetCommandLineArgs());
+            
+            if (additiveArgs != null)
+            {
+                ret.AddRange(additiveArgs);
+            }
+
+            return ret;
+        }
+
+        public static void AddCommandLineArgs(params string[] args)
+        {
+            if (additiveArgs == null)
+            {
+                additiveArgs = new List<string>();
+            }
+
+            foreach (var argument in args)
+            {
+                additiveArgs.Add(argument);
+            }
+
+        }
+
+    }
+}
